1717d8f2ff7bcb244395ab63116b4d2d63fb7f12
Seb Dalgarno <seb@poissonconsulting.ca>
2019-05-29 23:45:34 -0700

    navbar css from shinyutils

--- R/app_ui.R
+++ R/app_ui.R
@@ -1,25 +1,11 @@
 #' @import shiny
 app_ui <- function() {
   tagList(
-    # Leave this function for adding external resources
-    # from inst/app/www
-    golem_add_external_resources(),
-    # List the first level UI elements here 
-    fluidPage(
-      h1("slobr")
-    )
-  )
-}
-
-golem_add_external_resources <- function(){
-  
-  addResourcePath(
-    'www', system.file('app/www', package = 'slobr')
-  )
- 
-  tagList(
-    # Add here all the external resources
-    # If you have a custom.css in the inst/app/www
-    #tags$link(rel="stylesheet", type="text/css", href="www/custom.css")
+    shinyutils::css_navbar(),
+    navbarPage(title =  "Slobr", selected = 'Slob',
+               tabPanel(title = "Slob",
+                        mod_slobr_ui("ui_slobr_1")),
+               tabPanel(title = 'About',
+                        shinyutils::mod_about_ui("ui_about_1")))
   )
 }
